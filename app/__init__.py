6e5c899d8e7964c044555420ff90f52576b2afa7
Dan Marl Cadiz <danmarl.cadiz@g.msuiit.edu.ph>
2022-01-22 10:16:00 +0800

    For Cloudinary Config

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from flask_mysql_connector import MySQL
 from flask_bootstrap import Bootstrap
 from config import DB_USERNAME, DB_PASSWORD, DB_NAME, DB_HOST, SECRET_KEY, BOOTSTRAP_SERVE_LOCAL
+from config import  CLOUD_NAME, API_KEY, API_SECRET
 from flask_wtf.csrf import CSRFProtect
 import cloudinary
 import cloudinary.uploader
@@ -26,9 +27,9 @@ def create_app(test_config=None):
         #BOOTSTRAP_SERVE_LOCAL=BOOTSTRAP_SERVE_LOCAL
     )
     cloudinary.config(
-        cloud_name="dlcszgqhi",
-        api_key="782191683721864",
-        api_secret="iWNacCpwBmna_4scMPzubvy3xFs"
+        CLOUD_NAME=CLOUD_NAME,
+        API_KEY=API_KEY,
+        API_SECRET=API_SECRET
     )
 
     bootstrap.init_app(app)
